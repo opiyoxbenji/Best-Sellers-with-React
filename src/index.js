7d9b8c49b16863e2f60235b83b10a9606a9feacf
opiyoxbenji <opiyosbenji@gmail.com>
2023-03-21 10:34:25 +0300

    adding styling to the booklist component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React from 'react'; //for libraries
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './index.css'; //for our own folders or files
+
+const BookList = () => {
+    return (
+        <section className='booklist'>
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+};
+
+const Book = () => {
+    return (
+        <article className='book'>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
+};
+
+const Image = () => (
+    <h2>
+        <img
+            src='https://images-na.ssl-images-amazon.com/images/I/813d0G3ILGL._AC_UL300_SR300,200_.jpg'
+            alt='Young Forever'
+        />
+    </h2>
+);
+const Title = () => <h2>Young Forever</h2>;
+const Author = () => {
+    return <h4>Dr. Mark Hyman MD </h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
