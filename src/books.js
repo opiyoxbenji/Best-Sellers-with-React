5920e6da655f12386ba1fc4b849eae5d334c43c5
opiyoxbenji <opiyosbenji@gmail.com>
2023-03-23 10:58:30 +0300

    split components into files and imported to index

---
+++ src/books.js
@@ -0,0 +1,31 @@
+export const books = [
+    {
+        author: 'Dr. Mark Hyman MD',
+        title: 'Young Forever',
+        img: './images/young_forever.jpg',
+        id: 1,
+    },
+
+    {
+        author: 'Colleen Hoover',
+        title: 'It Ends with Us',
+        img: './images/it-ends-with-us.jpg',
+        id: 2,
+    },
+
+    {
+        author: 'Bonnie Garmus',
+        title: 'Lessons in Chemistry',
+        img: './images/lessons-in-chem.jpg',
+        id: 3,
+    },
+
+    {
+        author: 'Colleen Hoover',
+        title: 'It Starts with Us',
+        img: './images/it-starts-with-us.jpg.jpg',
+        id: 4,
+    },
+];
+
+
--- src/index.js
+++ src/index.js
@@ -2,70 +2,68 @@ import React from 'react'; //for libraries
 import ReactDOM from 'react-dom/client';
 
 import './index.css'; //for our own folders or files
-const firstBook = {
-    author: 'Dr. Mark Hyman MD',
-    title: 'Young Forever',
-    img: './images/young_forever.jpg',
-};
-
-const secondBook = {
-    author: 'Colleen Hoover',
-    title: 'It Ends with Us',
-    img: './images/it-ends-with-us.jpg',
-};
-
-const thirdBook = {
-    author: 'Bonnie Garmus',
-    title: 'Lessons in Chemistry',
-    img: './images/lessons-in-chem.jpg',
-};
-
-const fourthBook = {
-    author: 'Colleen Hoover',
-    title: 'It Starts with Us',
-    img: './images/it-starts-with-us.jpg.jpg',
-};
+import { books } from './books'; // {} for getting a specific item
+import Book from './book';
 
 const BookList = () => {
     return (
         <section className='booklist'>
-            <Book
-                author={firstBook.author}
-                title={firstBook.title}
-                img={firstBook.img}
-            />
-            <Book
-                author={secondBook.author}
-                title={secondBook.title}
-                img={secondBook.img}
-            />
-            <Book
-                author={thirdBook.author}
-                title={thirdBook.title}
-                img={thirdBook.img}
-            />
-            <Book
-                author={fourthBook.author}
-                title={fourthBook.title}
-                img={fourthBook.img}
-            />
+            {books.map(book => {
+                //with the spread op
+                return <Book {...book} key={book.id} />;
+            })}
         </section>
-    );
-};
-
-const Book = props => {
-    const { img, title, author } = props; //using destructuring
-    return (
-        <article className='book'>
-            <img src={img} alt={title} />
-            <h2>{title}</h2>
-            <h4>{author}</h4>
-        </article>
     );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList />);
+
+// before book
+// const EventExamples = () => {
+// const handleFormInput = e => {
+//     console.log(e.target);
+//     console.log(e.target.name);
+//     console.log(e.target.value);
+// };
+// const handleButtonClick = () => {
+//     alert('Handle Button Click');
+// };
+// const handleFormSubmission = e => {
+//     e.preventDefault(); //we will handle form submissions
+//     console.log('form submitted');
+// };
+//     return (
+//         <section>
+//             {/* add onSubmit Event Handler */}
+//             <form onSubmit={handleFormSubmission}>
+//                 <h2>Typical Form</h2>
+//                 <input
+//                     type='text'
+//                     name='example'
+//                     onChange={handleFormInput}
+//                     style={{ margin: '1rem 0' }}
+//                 />
+//                 {/* add button with type='submit' */}
+//                 <button type='submit'>submit form</button>
+//             </form>
+//             <button onClick={handleButtonClick}>clicky </button>
+//         </section>
+//     );
+// };
+
+//after book
+//adding the children prop
+// const Book = ({ img, title, author, children }) => {
+//     return (
+//         <article className='book'>
+//             <img src={img} alt={title} />
+//             <h2>{title}</h2>
+//             <h4>{author}</h4>
+//             {children}
+//         </article>
+//     );
+// };
 
 /* <h4 style={{color: '#617d98',fontSize: '0.75rem',marginTop: '0.5rem',}}></h4> */
 
@@ -87,3 +85,9 @@ root.render(<BookList />);
 //         <h4 style={inlineHeadingStyles}>Dr. Mark Hyman MD </h4>
 //     ); //CSS inline styles
 // };
+
+//array map //is declarative -> use functions to describe state
+// const users = [{ name: 'illenium' }, { name: 'khali' }, { name: 'chain' }];
+
+// const usernames = users.map(user => user.name);
+// console.log(usernames)
